cdc8498a4aedd48a6743409198c6aeaf00eb3cf0
Dan Do <dxd3vmb@virginia.edu>
2021-11-01 21:30:07 -0400

    Files

---
+++ miko.py
@@ -0,0 +1,217 @@
+import discord
+
+import random
+import requests
+from discord.ext import tasks
+from keep_alive import keep_alive
+
+client = discord.Client()
+
+isActive = True
+
+game_list = [
+    "Super Mario World 2: Yoshi's Island",
+    "Yoshi's Story",
+    "Yoshi's Universal Gravitation",
+    "Yoshi Touch & Go",
+    "Yoshi's Island DS",
+    "Yoshi's New Island",
+    "Yoshi's Woolly World",
+    "Yoshi's Crafted World",
+    "VALORANT",
+    "League of Legends",
+]
+
+insults_keywords = [
+    'crazy',
+    'funny',
+    'poop',
+    'huge',
+    'dumb',
+    'stupid',
+    'stoopid',
+    'ugly',
+    'big',
+    'sad',
+    'fat',
+    'apple',
+    'weird',
+    'scary',
+    'buffalo',
+]
+
+insults_output = [
+    "ur face is ",
+    "u look like a ",
+    "boi ur forehead is ",
+    "ur mum is ",
+    "boi ur face is ",
+    "boi ya look like a ",
+]
+
+greetings = [
+    "Ooga! Yoshi boogie!",
+    "Yoshi! Yoshi!",
+    "YOSHI, FAIRY OF DRAGON FLAME!!!",
+    "Oogabooga! I ready, and hungry!",
+    "YOSHI TO RESCUE!!!",
+]
+
+goodbyes = [
+    "Scooze me!",
+    "This means war!",
+    "Nooooooo",
+    "Ohhhh.... Yoshi no feel good!",
+    "Aww, do I have to go to bed so soon?",
+    "Night, Mama Luigi!",
+    "Hey! I still hungry!",
+]
+
+roast_blacklist = [
+    "758086695949434911",
+    "316687901884678144",
+    "669660483833561110",
+]
+
+whitelist = [
+    "333025781309767684",
+]
+
+
+# ---------------------------------------------------------------------------------------------------------------------------
+def random_game_status():
+    return discord.Game(name=random.choice(game_list))
+
+
+def random_song_status():
+    return discord.Activity(type=discord.ActivityType.listening, name="Spotify")
+
+
+def get_insults(word):
+    return random.choice(insults_output) + word
+
+
+def get_insp_quote(person):
+    omit = ["i", "me", "he", "she", "her", "him"]
+    json_res = requests.get("https://zenquotes.io/api/random").json()
+    quote = json_res[0]['q']
+    # author = json_res[0]['a']
+    inspire = "{0}".format(quote)
+    return person.capitalize() + ". " + inspire if person.lower() not in omit else "" + inspire
+
+
+def get_roasted(person):
+    omit = ["天", "yoshii", "yoshi", "i", "me", "he", "she", "her", "him", "thien", "aden", "thein", "thienn", "adenn",
+            "theinn"]
+    repsonses = ["nah", "no", "try again", "wut"]
+    if person.lower() in omit:
+        return random.choice(repsonses)
+    else:
+        return requests.get("https://insult.mattbas.org/api/en/insult.txt?who=" + person.capitalize()).text
+
+
+def photo_searcher_cat():
+    cat_photo_res = requests.get("https://api.thecatapi.com/v1/images/search").json()[0]
+    cat_photo_url = cat_photo_res['url']
+    cat_photo_width = cat_photo_res['width']
+    cat_photo_height = cat_photo_res['height']
+    cat_fact = requests.get("https://catfact.ninja/fact").json()['fact']
+    embed = discord.Embed(
+        title='Fun Fact 🐈',
+        description=cat_fact,
+        colour=discord.Colour.purple(),
+        width=cat_photo_width,
+        height=cat_photo_height
+    )
+    embed.set_image(url=cat_photo_url)
+    embed.set_footer(text="")
+    return embed
+
+
+def get_joke():
+    return requests.get("https://v2.jokeapi.dev/joke/Any?type=single").json()['joke']
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------------
+
+
+@client.event
+async def on_message(message):
+    global isActive
+
+    # Check status
+    if message.content.startswith("yoshii status"):
+        await message.channel.send("I'm awake!" if isActive else "I'm asleep...zZzZzZ")
+
+    # Set awake or sleep
+    if message.content.startswith("yoshii wake up"):
+        isActive = True
+        await message.channel.send(random.choice(greetings))
+    elif message.content.startswith("yoshii sleep"):
+        isActive = False
+        await client.change_presence(status=discord.Status.idle, activity=None)
+        await message.channel.send(random.choice(goodbyes))
+
+    # Do these only when active
+    if isActive:
+        # print(message.author.name, message.author.id, type(message.author.id))
+        # Prevent infinite loop
+        if message.author == client.user:
+            return
+
+        # Add to insults_keywords
+        elif message.content.startswith("yoshii add "):
+            await message.channel.send(message.content.split(' ')[-1] + " added to keywords")
+            if message.content.split(' ')[-1] not in insults_keywords:
+                insults_keywords.append(message.content.split(' ')[-1])
+
+        # Remove from insults_keywords
+        elif message.content.startswith("yoshii remove "):
+            await message.channel.send(message.content.split(' ')[-1] + " removed from keywords")
+            if message.content.split(' ')[-1] in insults_keywords:
+                insults_keywords.remove(message.content.split(' ')[-1])
+
+        # Random inspirational quotes
+        elif message.content.startswith("yoshii inspire "):
+            await message.channel.send(get_insp_quote(message.content.split(' ')[-1]))
+
+        # Random roasts
+        elif message.content.startswith("yoshii roast "):
+            if str(message.author.id) not in roast_blacklist:
+                await message.channel.send(get_roasted(message.content.split(' ')[-1]))
+            else:
+                await message.channel.send("nah, ur banned")
+
+        # Random cat photos
+        elif message.content.startswith("yoshii cat"):
+            await message.channel.send(embed=photo_searcher_cat())
+
+        # Random joke
+        elif message.content.startswith("yoshii tell a joke"):
+            await message.channel.send(get_joke())
+
+        # Output the list of insults_keywords
+        elif message.content.startswith("yoshii keywords"):
+            await message.channel.send(insults_keywords)
+
+        # Default greetings
+        elif message.content.endswith("yoshii"):
+            await message.channel.send(random.choice(greetings))
+
+
+        # Last condition
+        else:
+            for word in insults_keywords:
+                if word in message.content.lower():
+                    ran_num = random.randint(1, 1)
+                    if ran_num == 1:
+                        if (str(message.author.id) not in whitelist):
+                            await message.channel.send(get_insults(word))
+
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------------
+keep_alive()
+client.run(('ODgwNTYyMzcxNjkxNjg3OTU4.YSgFnQ.rpoTCm_vT7nEO1TY1qRayXxu6Hg'))
---
+++ miko_basic.py
@@ -0,0 +1,139 @@
+import discord
+import os
+import random
+import requests
+#from keep_alive import keep_alive
+
+client = discord.Client()
+
+isActive = True
+
+homeworks = {}
+
+insults_output = []
+
+greetings = [
+    "おはようございます！",
+    "こんにちは",
+    "さしぶりね"
+]
+
+goodbyes = [
+    "さようなら",
+    "失礼します",
+    "じゃあ、まった",
+    "まったね!",
+    "ふん。。ふん",
+
+]
+
+
+# ---------------------------------------------------------------------------------------------------------------------------
+
+def get_homework():
+    s = ""
+    for key in homeworks:
+        s += key + " " + homeworks[key]
+        s += "\n"
+    return s
+
+
+def get_insp_quote(person):
+    omit = ["i", "me", "he", "she", "her", "him"]
+    json_res = requests.get("https://zenquotes.io/api/random").json()
+    quote = json_res[0]['q']
+    # author = json_res[0]['a']
+    inspire = "{0}".format(quote)
+    return person.capitalize() + ". " + inspire if person.lower() not in omit else "" + inspire
+
+
+
+def photo_searcher_cat():
+    cat_photo_res = requests.get("https://api.thecatapi.com/v1/images/search").json()[0]
+    cat_photo_url = cat_photo_res['url']
+    cat_photo_width = cat_photo_res['width']
+    cat_photo_height = cat_photo_res['height']
+    cat_fact = requests.get("https://catfact.ninja/fact").json()['fact']
+    embed = discord.Embed(
+        title='Fun Fact 🐈',
+        description=cat_fact,
+        colour=discord.Colour.purple(),
+        width=cat_photo_width,
+        height=cat_photo_height
+    )
+    embed.set_image(url=cat_photo_url)
+    embed.set_footer(text="")
+
+    return embed
+
+
+# ---------------------------------------------------------------------------------------------------------------------------
+
+@client.event
+async def on_ready():
+    print("Logged in as {0.user}".format(client))
+
+
+@client.event
+async def on_message(message):
+    global isActive
+
+    # Check status
+    if message.content.startswith("Miko status"):
+        await message.channel.send("I'm awake!" if isActive else "I'm asleep...zZzZzZ")
+
+    # Set awake or sleep
+    if message.content.startswith("Miko wake up"):
+        isActive = True
+        await message.channel.send(random.choice(greetings))
+    elif message.content.startswith("Miko sleep"):
+        isActive = False
+        await message.channel.send(random.choice(goodbyes))
+
+    if isActive:
+        # print(message.author.name)
+        # Prevent infinite loop
+        if message.author == client.user:
+            return
+
+        # Add to insults_keywords
+        elif message.content.startswith("Miko add homework "):
+            print(message.content)
+            await message.channel.send(message.content.split(' ')[-2] + " added to homework")
+            if message.content.split(' ')[-1] not in homeworks:
+                homeworks[message.content.split(' ')[-2]] = message.content.split(' ')[-1]
+
+        elif message.content.startswith("Miko homework"):
+            if homeworks:
+                await message.channel.send(get_homework())
+            else:
+                await message.channel.send("No homework tonight!")
+
+        # Remove from insults_keywords
+        elif message.content.startswith("Miko remove homework "):
+            await message.channel.send(message.content.split(' ')[-1] + " removed from homework")
+            if message.content.split(' ')[-1] in homeworks:
+                del homeworks[message.content.split(' ')[-2]]
+            else:
+                await message.channel.send("Please put in an existing homework")
+
+        # Random inspirational quotes
+        elif message.content.startswith("Miko inspire "):
+            await message.channel.send(get_insp_quote(message.content.split(' ')[-1]))
+
+        # Random cat photos
+        elif message.content.startswith("Miko cat"):
+            await message.channel.send(embed=photo_searcher_cat())
+
+        # Output the list of insults_keywords
+        # Default greetings
+
+        # Last condition
+        # Listening for insults_keywords
+
+
+# ---------------------------------------------------------------------------------------------------------------------------
+#keep_alive()
+#client.run(os.getenv('ODgwNTYyMzcxNjkxNjg3OTU4.YSgFnQ.rpoTCm_vT7nEO1TY1qRayXxu6Hg'))
+
+client.run(('ODgwNTYyMzcxNjkxNjg3OTU4.YSgFnQ.rpoTCm_vT7nEO1TY1qRayXxu6Hg'))
